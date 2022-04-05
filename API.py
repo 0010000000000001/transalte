8ef90c7bb744133c44b0fa8486174f6e738b105d
Roman Romanov <95626629+0010000000000001@users.noreply.github.com>
2022-04-05 14:58:20 +0200

    take to use

---
+++ API.py
@@ -0,0 +1,6 @@
+#   take to use
+api = 'MmNkNjI5OWUtZTRiMS00Nzc5LTk0N2YtODE2YTNiNDBiOWVkOjFlYWU3YjNlZWJhNjRjYTk4NTJlMWU2MGMxMDJlMGFi'
+
+url = 'https://developers.lingvolive.com/api/v1.1/authenticate'
+
+trs = 'https://developers.lingvolive.com/api/v1/Minicard'
